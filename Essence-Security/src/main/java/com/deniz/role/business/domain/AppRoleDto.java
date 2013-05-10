736a296736f2820685d736983be3596043c102cc
deniz.yavas <denizyavas@gmail.com>
2013-05-10 16:23:06 +0300

    Initial Commit

---
+++ Essence-Security/src/main/java/com/deniz/role/business/domain/AppRoleDto.java
@@ -0,0 +1,48 @@
+package com.deniz.role.business.domain;
+
+import com.deniz.framework.dto.AbstractDto;
+import com.deniz.framework.dto.Meta;
+import com.deniz.framework.dto.ValueType;
+import com.deniz.framework.dto.Meta.ValueMode;
+
+
+public class AppRoleDto extends AbstractDto
+{
+
+	private Meta roleNameMeta = new Meta();
+	private Meta roleGroupMeta = new Meta();
+	private Meta domainMeta = new Meta();
+	private Meta loginRequiredMeta = new Meta();
+
+
+	public Meta getRoleNameMeta()
+	{
+		return roleNameMeta;
+	}
+
+
+	public Meta getRoleGroupMeta()
+	{
+		return roleGroupMeta;
+	}
+
+
+	public Meta getDomainMeta()
+	{
+		return domainMeta;
+	}
+
+
+	public Meta getLoginRequiredMeta()
+	{
+		return loginRequiredMeta;
+	}
+
+
+	@Override
+	public String getInfo()
+	{
+		return roleNameMeta.getValue();
+	}
+
+}
